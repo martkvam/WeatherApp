10afa2530bcc0cd41e098959514582299f55eae0
Martin Kvam <martin.kvam@inmeta.no>
2024-10-28 09:42:32 +0100

    Init details screen for detailed weather report

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,6 +31,13 @@ export default function RootLayout() {
     <ThemeProvider value={DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="details"
+          options={{
+            headerTitle: "Weather details",
+            headerBackTitleVisible: false,
+          }}
+        />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
---
+++ app/details.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function WeatherDetails() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Her kommer værdetaljer</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
