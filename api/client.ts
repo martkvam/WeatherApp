6742ba2ae4c50923e49dab7af9d3c9cd2669bb16
Martin Kvam <martin.kvam@inmeta.no>
2024-10-30 01:00:31 +0100

    sunApiClient

--- api/client.ts
+++ api/client.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-export const apiClient = axios.create({
+export const weatherApiClient = axios.create({
   baseURL: "https://api.met.no/weatherapi/locationforecast/2.0/compact",
 });
+
+export const sunApiClient = axios.create({
+    baseURL: "https://api.met.no/weatherapi/sunrise/3.0/sun",
+})
