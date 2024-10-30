7ba99bfd6942cfce17e7381750fa74c3d532a771
Martin Kvam <martin.kvam@inmeta.no>
2024-10-30 01:01:35 +0100

    Current location in useLocations

--- hooks/useLocations.ts
+++ hooks/useLocations.ts
@@ -1,5 +1,6 @@
-import { Coordinates } from "@/types/coordinates"
-import { useState } from "react"
+import { Coordinates } from "@/types/coordinates";
+import { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 const defaultLocations: Coordinates[] = [{
     name: "Berlin",
@@ -12,10 +13,30 @@ const defaultLocations: Coordinates[] = [{
     long: -0.1180
 }];
 
-const useLocations = () => {
+export const useLocations = () => {
     const [locations, setLocations] = useState<Coordinates[]>(defaultLocations);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                return;
+            }
+
+            const location = await Location.getCurrentPositionAsync({});
+            const latitude = Number((location.coords.latitude).toFixed(4)); 
+            const longitude = Number((location.coords.longitude).toFixed(4));
+            const currentPosition: Coordinates = {
+                name: "My location",
+                lat: latitude,
+                long: longitude
+            } 
+            let newLocationList = locations.filter(x => x.name !== "My location");
+            newLocationList.unshift(currentPosition);
+            setLocations(newLocationList);
+            }
+        )();
+    }, [])
 
     return locations;
 }
-
-export default useLocations;
